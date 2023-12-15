7d0a0a1fe11f9816ea2a94dc9fe4f6b60f6cad9d
minhnhut1712 <nminhnhut0112@gmail.com>
2023-12-15 21:24:26 +0700

    connect mongo and config env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,27 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from './environment'
+
+let todolistDataBaseInstance = null
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+
+  todolistDataBaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!todolistDataBaseInstance) throw new Error('Must Connect to Database first!')
+  return todolistDataBaseInstance
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,41 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from './config/mongodb'
+import { env } from './config/environment'
 
-const app = express()
+const START_SEREVR = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Server is running at http://${env.APP_HOST}:${env.APP_PORT}/`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  exitHook(() => {
+    CLOSE_DB()
+    console.log('Exit app. Goodbye!!')
+  })
+}
+
+;(async () => {
+  try {
+    await CONNECT_DB()
+    console.log('Connected to MongoDb!')
+    START_SEREVR()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDb'))
+//   .then(() => START_SEREVR())
+//   .catch((error) => {
+//     console.error(error)
+//     process.exit(0)
+//   })
